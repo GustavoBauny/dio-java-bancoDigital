dfb4a6e36f45dabb68decbea5fb6c1fb193b9a55
GustavoBauny <gustavounipam1504@gmail.com>
2022-05-20 14:03:23 -0300

    Adicionando mudança no nome do cliente

--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@
 public class Main {
 
 	public static void main(String[] args) {
-		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
+		Cliente gustavo = new Cliente();
+		gustavo.setNome("Gustavo");
 		
-		Conta cc = new ContaCorrente(venilton);
-		Conta poupanca = new ContaPoupanca(venilton);
+		Conta cc = new ContaCorrente(gustavo);
+		Conta poupanca = new ContaPoupanca(gustavo);
 
 		cc.depositar(100);
 		cc.transferir(100, poupanca);
